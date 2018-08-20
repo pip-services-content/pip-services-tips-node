8fd6dd867db01168c040994802900c1693267b26
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:09:10 +0300

    added DefaultRpcFactory

--- obj/src/container/TipsProcess.js
+++ obj/src/container/TipsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const TipsServiceFactory_1 = require("../build/TipsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class TipsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("tips", "User tips microservice");
         this._factories.add(new TipsServiceFactory_1.TipsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.TipsProcess = TipsProcess;
--- src/container/TipsProcess.ts
+++ src/container/TipsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { TipsServiceFactory } from '../build/TipsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class TipsProcess extends ProcessContainer {
 
     public constructor() {
         super("tips", "User tips microservice");
         this._factories.add(new TipsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
